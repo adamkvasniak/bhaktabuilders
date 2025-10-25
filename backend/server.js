cf8eceab0be1946a9d40232dc919d95c5ef5e0e8
Adam Kvasniak <adamixa@Adams-MacBook-Pro-2.local>
2025-10-25 16:05:24 +0200

    pici co ja vim, asi volakyu server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+const allowedOrigins = process.env.CORS_ORIGIN.split(","); // converts string to array
 
 app.use(
   cors({
